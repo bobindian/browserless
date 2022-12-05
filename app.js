fff4d30e7e6c42f103d4980d70b1530596f87d27
bobindian <bobindian007@gmail.com>
2022-12-06 00:30:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,17 +8,17 @@ const browser = createBrowser()
 // After that, you can create as many browser context
 // as you need. The browser contexts won't share cookies/cache
 // with other browser contexts.
-const browserless = await browser.createContext()
+const browserless = browser.createContext()
 
 // Perform the action you want, e.g., getting the HTML markup
-const buffer = await browserless.screenshot('http://example.com', {
+const buffer = browserless.screenshot('http://example.com', {
   device: 'iPhone 6'
 })
 
 console.log(termImg(buffer))
 
 // After your task is done, destroy your browser context
-await browserless.destroyContext()
+ browserless.destroyContext()
 
 // At the end, gracefully shutdown the browser process
-await browser.close()
+ browser.close()
